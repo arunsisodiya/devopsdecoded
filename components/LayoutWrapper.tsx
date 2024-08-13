88d5ae3c4b8624ea3cffbca996b0770b538e2b7c
Arun Singh Sisodiya <arun.singh@mercedes-benz.io>
2024-08-13 09:54:31 +0200

    Adding fonts and code styling

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,22 +1,37 @@
-import { Karla } from '@next/font/google';
-
 import SectionContainer from './SectionContainer';
 import Footer from './Footer';
 import { ReactNode } from 'react';
 import Header from './Header';
+import localFont from '@next/font/local';
 
 interface Props {
   children: ReactNode;
 }
 
-const inter = Karla({
-  subsets: ['latin'],
+const wotfard = localFont({
+  src: [
+    {
+      path: './fonts/wotfard/wotfard-medium-webfont.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/wotfard/wotfard-regular-webfont.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/wotfard/wotfard-semibold-webfont.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
 });
 
 const LayoutWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
-      <div className={`${inter.className} relative flex h-screen flex-col justify-between font-sans`}>
+      <div className={`${wotfard.className} relative flex h-screen flex-col justify-between font-sans`}>
         <Header />
         <main className="mb-auto mt-20">{children}</main>
         <Footer />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/css/tailwind.css';
 import '@/css/twemoji.css';
 import '@/css/resume.css';
 import '@/css/about.css';
-import '@/css/prism.css';
+// import '@/css/prism.css';
 import 'katex/dist/katex.css';
 // import '@/css/docsearch.css' // Uncomment if using algolia docsearch
 // import '@docsearch/css' // Uncomment if using algolia docsearch
